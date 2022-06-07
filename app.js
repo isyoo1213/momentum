87965ea4b32a5b109f6f49c0fb8d80db06f21385
isyoo1213 <moethns@gmail.com>
2022-06-08 00:54:59 +0900

    feat: add files for Controlling HTMl by JS

---
+++ app.js
@@ -0,0 +1,8 @@
+const title = document.getElementById("title");
+
+title.id = "Id by JS"
+title.innerText = "Inner Text by JS";
+title.className = "ClassName by JS"
+console.log(title.id);
+console.log(title.innerText);
+console.log(title.className);
