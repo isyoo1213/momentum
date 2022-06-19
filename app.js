9187120e3933eb6c6ae69e4fcf36e01c7ea4bdce
isyoo1213 <moethns@gmail.com>
2022-06-19 13:28:32 +0900

    feat: add saving Username by JS in LocalStorage

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-const title = document.getElementById("title");
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
 
-title.id = "Id by JS"
-title.innerText = "Inner Text by JS";
-title.className = "ClassName by JS"
-console.log(title.id);
-console.log(title.innerText);
-console.log(title.className);
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "user_name";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+if(savedUsername == null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
