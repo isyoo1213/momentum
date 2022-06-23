8bead79389042d7fff1197d461272af379747322
isyoo1213 <moethns@gmail.com>
2022-06-23 19:23:10 +0900

    feat: add Bring and show location and weather by API

--- weather.js
+++ weather.js
@@ -2,8 +2,20 @@ function onGeoIsOk(position){
     const lat = position.coords.latitude;
     const lon = position.coords.longitude;
     console.log("you live at ", lat, lon);
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
     console.log(url);
+    // fetch는 promise
+    fetch(url)
+    .then(response => response.json()
+        .then(data =>{
+            const cityName = document.querySelector(".weather strong:first-child");
+            const cityWeather = document.querySelector(".weather strong:last-child") 
+            cityName.innerText = data.name;
+            cityWeather.innerText = data.weather[0].main;
+            }
+        ) 
+    )
+    
 }
 
 function onGeoIsError(){
